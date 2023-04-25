23b8647dc6ce05c439b165af7d956ceb0df3d0d9
Diego-coder2525 <forgam2525@outlook.es>
2023-04-24 23:17:02 -0500

    Mejora de nav, rutas

--- src/main/java/com/pizzeria/cibertec/Pizzeria/controller/HomeController.java
+++ src/main/java/com/pizzeria/cibertec/Pizzeria/controller/HomeController.java
@@ -9,13 +9,41 @@ import org.springframework.web.bind.annotation.RestController;
 // @RestController
 @Controller
 public class HomeController {
-        // Padre del @GetMapping
+    // Padre del @GetMapping
         /*@RequestMapping("/home")
         public String home(){
             return "ASDADSASDASDDASADASDDDDDDDDDDDD";
         }*/
-        @GetMapping ("/inicio")
-        public String inicio(){
-            return "inicio";
-        }
+    @GetMapping("/inicio")
+    public String inicio() {
+
+        return "inicio";
+    }
+
+    @GetMapping("/registro")
+    public String registro() {
+        return "registro";
+    }
+
+    @GetMapping("/reservas")
+    public String reservas() {
+        return "reservas";
+    }
+
+    @GetMapping("/pedidos")
+    public String pedidos() {
+        return "pedidos";
+    }
+    @GetMapping("/historial")
+    public String historial() {
+        return "historial";
+    }
+    @GetMapping("/contacto")
+    public String contacto() {
+        return "contacto";
+    }
+    @GetMapping("/carrito")
+    public String carrito() {
+        return "carrito";
+    }
 }
